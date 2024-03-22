ba3de76ecffa7807b1f1a123bf07e9164ffe00b1
John Fiumara <fumarajohn8@gmail.com>
2024-03-22 16:08:42 -0400

    Update StarBackground.tsx

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -38,7 +38,7 @@ const StarBackground = (props: any) => {
   )
 };
 
-export default const StarsCanvas = ()  =>(
+export const StarsCanvas = ()  =>(
     <div className='w-full h-auto fixed inset-0 z-[20]'>
         <Canvas camera={{position: [0,0,1]}}>
             <Suspense fallback={null}>
